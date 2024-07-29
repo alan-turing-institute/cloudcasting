4312edea8faa4e3d1e4fd916a6f3b711e7f71f84
Nathan Simpson <nsimpson@turing.ac.uk>
2024-07-29 12:00:36 +0100

    Revert "remove netcdf dependency"
    This reverts commit d6770aae0382f7e7a482e9a222d825f3da705dde.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 
 import numpy as np
 import pandas as pd
@@ -14,33 +13,30 @@ def temp_output_dir(tmp_path):
     return str(tmp_path)
 
 
-@pytest.fixture(scope="session")
-def sat_zarr_path():
-    with tempfile.TemporaryDirectory() as tempdir:
-        # Load dataset which only contains coordinates, but no data
-        ds = xr.open_zarr(
-            f"{os.path.dirname(os.path.abspath(__file__))}/test_data/non_hrv_shell.zarr.zip"
-        )
+@pytest.fixture()
+def sat_zarr_path(temp_output_dir):
+    # Load dataset which only contains coordinates, but no data
+    ds = xr.load_dataset(
+        f"{os.path.dirname(os.path.abspath(__file__))}/test_data/non_hrv_shell.netcdf"
+    )
 
-        # Add time coord
-        ds = ds.assign_coords(
-            time=pd.date_range("2023-01-01 00:00", "2023-01-02 23:55", freq="5min")
-        )
+    # Add time coord
+    ds = ds.assign_coords(time=pd.date_range("2023-01-01 00:00", "2023-01-02 23:55", freq="5min"))
 
-        # Add data to dataset
-        ds["data"] = xr.DataArray(
-            np.zeros([len(ds[c]) for c in ds.coords]),
-            coords=ds.coords,
-        )
+    # Add data to dataset
+    ds["data"] = xr.DataArray(
+        np.zeros([len(ds[c]) for c in ds.coords]),
+        coords=ds.coords,
+    )
 
-        # Transpose to variables, time, y, x (just in case)
-        ds = ds.transpose("variable", "time", "y_geostationary", "x_geostationary")
+    # Transpose to variables, time, y, x (just in case)
+    ds = ds.transpose("variable", "time", "y_geostationary", "x_geostationary")
 
-        # Add some NaNs
-        ds["data"].values[:, :, 0, 0] = np.nan
+    # Add some NaNs
+    ds["data"].values[:, :, 0, 0] = np.nan
 
-        # Save temporarily as a zarr
-        zarr_path = f"{tempdir}/test_sat.zarr"
-        ds.to_zarr(zarr_path)
+    # Save temporarily as a zarr
+    zarr_path = f"{temp_output_dir}/test_sat.zarr"
+    ds.to_zarr(zarr_path)
 
-        yield zarr_path
+    return zarr_path

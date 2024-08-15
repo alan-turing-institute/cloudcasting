9125c6d7e4c04b5633005a30ee09675cde281c24
James Fulton <djamesfulton@yahoo.co.uk>
2024-08-15 12:41:12 +0000

    add tests for validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,7 +1,7 @@
 import pytest
 from conftest import PersistenceModel
 
-from cloudcasting.validation import score_model_on_all_metrics
+from cloudcasting.validation import score_model_on_all_metrics, validate, calc_mean_metrics
 
 ROLLOUT_STEPS_TEST = 12
 
@@ -35,3 +35,44 @@ def test_score_model_on_all_metrics(val_sat_zarr_path, model, nan_to_num):
         assert metric_array.shape == (
             ROLLOUT_STEPS_TEST,
         ), f"Metric {metric_name} has the wrong shape"
+
+
+def test_calc_mean_metrics():
+    # Create a test dictionary of metrics
+    test_metrics_dict = {
+        "mae": [1, 2, 3],
+        "mse": [4, 5, 6],
+    }
+
+    # Call the calc_mean_metrics function
+    mean_metrics_dict = calc_mean_metrics(test_metrics_dict)
+
+    # Check the expected keys are present
+    assert mean_metrics_dict.keys() == {"mae", "mse"}
+
+    # Check the expected values are present
+    assert mean_metrics_dict["mae"] == 2
+    assert mean_metrics_dict["mse"] == 5
+
+
+def test_validate(model, val_sat_zarr_path, mocker):
+
+    # Mock the wandb functions so they aren't run in testing
+    mocker.patch("wandb.login")
+    mocker.patch("wandb.init")
+    mocker.patch("wandb.config")
+    mocker.patch("wandb.log")
+    mocker.patch("wandb.plot.line")
+    mocker.patch("wandb.plot.bar")
+    
+
+    validate(
+        model=model,
+        data_path=val_sat_zarr_path,
+        wandb_project_name="cloudcasting-pytest",
+        wandb_run_name="test_validate",
+        nan_to_num = False,
+        batch_size = 2,
+        num_workers = 0,
+        num_termination_batches = 4,
+    )
